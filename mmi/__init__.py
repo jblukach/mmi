4b4da4f180a86aee8c225e5b504285527dff2e39
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-10-07 02:37:15 +0000

    mmi

--- mmi/__init__.py
+++ mmi/__init__.py
@@ -1,1 +1,25 @@
-__version__ = VERSION = '0.0.1'
+import hashlib
+import os
+import sys
+
+BLOCKSIZE = 65536
+path = os.path.dirname(__file__)
+
+__sha256__ = SHA256 = '0754EDF87A433AD0970C9EBB3CFD40F28390389FD9FF172FEB1AE6445E7701C4'
+__location__ = LOCATION = path[:-4]+'/data/mmi.bloom'
+__version__ = VERSION = '0'
+
+
+sha256_hasher = hashlib.sha256()
+with open(__location__,'rb') as f:
+    buf = f.read(BLOCKSIZE)
+    while len(buf) > 0:
+        sha256_hasher.update(buf)
+        buf = f.read(BLOCKSIZE)
+f.close()
+
+sha256_file = sha256_hasher.hexdigest().upper()
+
+if sha256_file != __sha256__:
+    print("MMI bloom filter is corrupted.")
+    sys.exit(1)
--- mmi/cli.py
+++ mmi/cli.py
@@ -1,2 +1,121 @@
+import asyncio
+import hashlib
+import os
+import pybloomfilter
+from mmi import __location__
+
+BLOCKSIZE = 65536
+
+async def check(sha256):
+    mmi = pybloomfilter.BloomFilter.open(__location__)
+    if sha256 in mmi:
+        value = 'YES'
+    else:
+        value = 'NO'
+    return value
+
+async def hasher(fullpath):
+    try:
+        sha256_hasher = hashlib.sha256()
+        with open(fullpath,'rb') as afile:
+            buf = afile.read(BLOCKSIZE)
+            while len(buf) > 0:
+                sha256_hasher.update(buf)
+                buf = afile.read(BLOCKSIZE)
+        sha256_file = sha256_hasher.hexdigest().upper()
+    except:
+        sha256_file = '                                                                '
+        pass
+    if sha256_file == 'E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855':
+        sha256_file = "\033[94m{}\033[00m" .format('** EMPTY **                                                     ')
+    status = await check(sha256_file)
+    if status == 'YES':
+        sha256_file = "\033[92m{}\033[00m" .format(sha256_file)
+    return sha256_file
+
+async def metahash(fullpath):
+    sha256_meta = hashlib.sha256()
+    sha256_meta.update(fullpath.encode())
+    sha256_hash = sha256_meta.hexdigest().upper()
+    status = await check(sha256_hash)
+    return status
+
+async def normalize(path):
+    if path[:1] == '/':
+        out = path.split('/')
+        try:
+            if out[1] == 'home':
+                out[2] = 'user'
+                path = '/'.join(out)
+        except:
+            pass
+    return path
+
+async def parsefilename(filename):
+    if filename[:1] == '/':
+        out = filename.split('/')
+        count = len(out) - 1
+        status = await metahash(out[count])
+    return status
+
+async def parseonlypath(onlypath):
+    if onlypath[:1] == '/':
+        out = onlypath.split('/')
+        del out[-1]
+        onlypath = '/'.join(out)
+        status = await metahash(onlypath)
+    return status
+
+async def start():
+    path = os.getcwd()
+    listing = os.listdir(path)
+    for list in listing:
+        isFile = os.path.isfile(path+'/'+list)
+        isDir = os.path.isdir(path+'/'+list)
+        if isFile == True:
+            try:
+                size = os.path.getsize(path+'/'+list)
+            except: 
+                size = 0
+                pass
+            if size == 0:
+                sha256 = "\033[94m{}\033[00m" .format('** EMPTY **                                                     ')
+            elif size > 104857599:
+                sha256 = "\033[91m{}\033[00m" .format('** LARGE **                                                     ')
+            else:
+                sha256 = await hasher(path+'/'+list)
+        elif isDir == True:
+            sha256 = ' -- DIR --                                                      '
+        if path == '/':
+            normalized = await normalize(path+list)
+        else:
+            normalized = await normalize(path+'/'+list)
+        fullpath = await metahash(normalized)
+        if fullpath == 'YES':
+            dir = "\033[96m{}\033[00m".format(path)
+            file = "\033[96m{}\033[00m".format(list)
+            slash = "\033[96m{}\033[00m".format('/')
+        elif fullpath == 'NO':
+            directory = await parseonlypath(normalized)
+            if directory == 'YES':
+                dir = "\033[97m{}\033[00m".format(path)
+            else:
+                dir = path
+            filename = await parsefilename(normalized)
+            if filename == 'YES':
+                file = "\033[97m{}\033[00m".format(list)
+            else:
+                file = list
+            slash = '/'
+        else:
+            dir = path
+            file = list
+            slash = '/'
+
+        if path == '/':
+            print(sha256+' '+dir+file)
+        else:
+            print(sha256+' '+dir+slash+file)
+
 def main():
-    print('Hello World!')
+    asyncio.run(start())
