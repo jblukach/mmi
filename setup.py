e86c2a67610ca0b6c745e670c8fc5324f47e31b9
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-10-09 08:23:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as f:
 setup(
     name = "matchmeta",
     version = __version__,
-    description = "Amazon Linux Analysis for Anyone and Everyone",
+    description = "Amazon Linux Triage for Anyone and Everyone",
     long_description = long_description,
     long_description_content_type = "text/markdown",
     url = "https://github.com/jblukach/mmi",
