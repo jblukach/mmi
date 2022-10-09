581b6f7ff44f783cd35a3c317ab7967303b37852
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-10-09 00:49:14 +0000

    1.1

--- mmi/__init__.py
+++ mmi/__init__.py
@@ -3,9 +3,10 @@ import os
 import sys
 
 BLOCKSIZE = 65536
+homedir = os.path.expanduser('~')
 
-if os.path.exists('~/.local/data/mmi.bloom') == True:
-    __location__ = LOCATION = '~/.local/data/mmi.bloom'
+if os.path.exists(homedir+'/.local/data/mmi.bloom') == True:
+    __location__ = LOCATION = homedir+'/.local/data/mmi.bloom'
 elif os.path.exists('/usr/local/data/mmi.bloom') == True:
     __location__ = LOCATION = '/usr/local/data/mmi.bloom'
 else:
@@ -18,7 +19,7 @@ __knownmeta__ = KNOWNMETA = '\033[96m{}\033[00m'        ### BLUE ###
 __largefile__ = LARGEFILE = '\033[91m{}\033[00m'        ### RED ###
 __partialmeta__ = PARTIALMETA = '\033[97m{}\033[00m'    ### GREY ###
 __sha256__ = SHA256 = 'AEA49CFAC9496A3CCD961BC9DD2B32CFCD8673963B37B79C506C537B78B4E56B'
-__version__ = VERSION = '1.0'
+__version__ = VERSION = '1.1'
 
 sha256_hasher = hashlib.sha256()
 with open(__location__,'rb') as f:
