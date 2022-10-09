de9e8e7741a4d51eaaf0ba7dcab07f73d6d7566a
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-10-09 00:10:54 +0000

    1.0.1

--- mmi/__init__.py
+++ mmi/__init__.py
@@ -1,18 +1,15 @@
 import hashlib
-import os
 import sys
 
 BLOCKSIZE = 65536
-path = os.path.dirname(__file__)
 
-__emptyfile__ = EMPTYFILE = '\033[94m{}\033[00m' ### PURPLE ###
-__knownfile__ = KNOWNFILE = '\033[92m{}\033[00m' ### GREEN ###
-__knownmeta__ = KNOWNMETA = '\033[96m{}\033[00m' ### BLUE ###
-__largefile__ = LARGEFILE = '\033[91m{}\033[00m' ### RED ###
-__location__ = LOCATION = path[:-4]+'/data/mmi.bloom'
-__partialmeta__ = PARTIALMETA = '\033[97m{}\033[00m' ### GREY ###
+__emptyfile__ = EMPTYFILE = '\033[94m{}\033[00m'        ### PURPLE ###
+__knownfile__ = KNOWNFILE = '\033[92m{}\033[00m'        ### GREEN ###
+__knownmeta__ = KNOWNMETA = '\033[96m{}\033[00m'        ### BLUE ###
+__largefile__ = LARGEFILE = '\033[91m{}\033[00m'        ### RED ###
+__partialmeta__ = PARTIALMETA = '\033[97m{}\033[00m'    ### GREY ###
 __sha256__ = SHA256 = 'AEA49CFAC9496A3CCD961BC9DD2B32CFCD8673963B37B79C506C537B78B4E56B'
-__version__ = VERSION = '1.0.0'
+__version__ = VERSION = '1.0.1'
 
 sha256_hasher = hashlib.sha256()
 with open(__location__,'rb') as f:
