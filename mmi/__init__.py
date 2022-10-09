5bb4b40f2e623ec454f50167d1e73d0e8b13f4e3
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-10-09 00:38:29 +0000

    1.0

--- mmi/__init__.py
+++ mmi/__init__.py
@@ -18,7 +18,7 @@ __knownmeta__ = KNOWNMETA = '\033[96m{}\033[00m'        ### BLUE ###
 __largefile__ = LARGEFILE = '\033[91m{}\033[00m'        ### RED ###
 __partialmeta__ = PARTIALMETA = '\033[97m{}\033[00m'    ### GREY ###
 __sha256__ = SHA256 = 'AEA49CFAC9496A3CCD961BC9DD2B32CFCD8673963B37B79C506C537B78B4E56B'
-__version__ = VERSION = '1.0.0'
+__version__ = VERSION = '1.0'
 
 sha256_hasher = hashlib.sha256()
 with open(__location__,'rb') as f:
