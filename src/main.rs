79df08d9ccaa15e71b176256506efa72d4d5ab9d
John Lukach <hello@lukach.io>
2025-02-09 23:11:10 +0000

    fix

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,6 @@ fn main() {
                         let directory = std::env::current_dir().unwrap();
                         let filename = entry.file_name();
                         let fullpath = directory.join(&filename);
-                        let metadata = std::fs::metadata(fullpath.clone()).unwrap();
-                        let fsize = metadata.len().to_string();
                         let b3dir;
                         let b3path;
                         let mut seperator: String;
@@ -45,22 +43,26 @@ fn main() {
                         let mut b3hash;
                         if fullpath.is_dir() {
                             b3hash = "  -- DIRECTORY --                                               ".to_string();
-                        } else if fsize == "0" {
-                            b3hash = "  -- ZERO --                                                    ".to_string();
-                        } else if fsize.parse::<u64>().unwrap() > 10*104857599 { // 1GB
-                            b3hash = "  -- LARGE --                                                   ".red().to_string();
                         } else {
                             let test = std::fs::File::open(entry.path());
                             if test.is_err() {
-                                b3hash = "  -- DENIED --                                                  ".red().to_string();
+                                b3hash = "  -- DENIED --                                                  ".red().to_string();                                
                             } else {
-                                b3hash = mmi::b3content(&fullpath);
-                                if b3hash == "af1349b9f5f9a1a6a0404dea36dcc9499bcb25c9adc112b7cc9a93cae41f3262" {
-                                    b3hash = "  -- EMPTY --                                                   ".to_string();
-                                } else if b3hash == "ERROR" {
-                                    b3hash = "  -- ERROR --                                                   ".red().to_string();
+                                let metadata = std::fs::metadata(fullpath.clone()).unwrap();
+                                let fsize = metadata.len().to_string();
+                                if fsize == "0" {
+                                    b3hash = "  -- ZERO --                                                    ".to_string();
+                                } else if fsize.parse::<u64>().unwrap() > 10*104857599 { // 1GB
+                                    b3hash = "  -- LARGE --                                                   ".red().to_string();
                                 } else {
-                                    b3hash = b3hash;
+                                    b3hash = mmi::b3content(&fullpath);
+                                    if b3hash == "af1349b9f5f9a1a6a0404dea36dcc9499bcb25c9adc112b7cc9a93cae41f3262" {
+                                        b3hash = "  -- EMPTY --                                                   ".to_string();
+                                    } else if b3hash == "ERROR" {
+                                        b3hash = "  -- ERROR --                                                   ".red().to_string();
+                                    } else {
+                                        b3hash = b3hash;
+                                    }
                                 }
                             }
                         }
